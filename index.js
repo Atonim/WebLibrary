19952f3fd3963aaf8071df11fdcc4fcca9c77737
Atonim <Atonimaurus@gmail.com>
2023-11-02 18:30:25 +0300

    colors and styles added

--- index.js
+++ index.js
@@ -3,27 +3,16 @@ import path from "path";
 import router from "./src/routes/servers.js";
 
 const __dirname = path.resolve();
-console.log('---------')
-console.log(__dirname)
-console.log('---------')
+
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.set('view engine', 'pug')
 app.set('views', './src')
-
+//app.use('/images', express.static('/images'));
 app.use(express.static(__dirname + '/assets'));
-app.use(router)
 
-//app.get('/', (req, res) => {
-//res.send('<h1>Hello</h1>');
-//res.sendFile(path.resolve(__dirname, 'static', 'index.html'));
-//res.render('pug/pages/index')
-//});
-//app.get('/features', (req, res) => {
-//res.send('<h1>Hello</h1>');
-//res.sendFile(path.resolve(__dirname, 'static', 'features.html'));
-//});
+app.use(router)
 
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}...`)
